a5ce3d4d4547df7018a1548de21054c41ae74bf0
Sacha Perry-Fagant <sacha.perry-fagant@umontreal.ca>
2026-08-14 16:02:48 -0400

    Website updates

--- src/CVPage.elm
+++ src/CVPage.elm
@@ -9,6 +9,7 @@ import Bytes exposing (Bytes)
 import Css.Global
 import Json.Encode
 
+
 -- MODEL
 type alias Model =
   { test : String
@@ -140,7 +141,7 @@ abstract = """Black holes in dimensions > 4 can take on new properties and topol
 viewAcademic : Html Msg
 viewAcademic =
   div [ css [margin4 (px 0) (px 60) (px 0) (px 30)]]
-  [ h3 [] [ text "My academic interests and experience lie in theoretical and computational physics"]
+  [ h3 [] [ text "My research interests are centred in cosmology, but I am equally fascinated by the laws of physics that govern the Universe."]
   , h2 [] [ text "Master's thesis:"]
   , h3 [] [ text "Abstract" ]
   , p [] [ text abstract]
@@ -153,13 +154,11 @@ viewWork : Html Msg
 viewWork =
   div [ css [ displayFlex, flexDirection column, justifyContent left, margin2 (px 0) (px 30)] ]
     [ h3 []
-      [ p [] [ text "I am currently employed as a software developer at Improving Ottawa, also known as ",
+      [ p [] [ text "My most recent role was as a software developer at Improving Ottawa, also known as ",
               a [ href "https://yoppworks.com/", Att.target "_blank" ] [ text "YoppWorks"]
               , text "."
               ]
-      , p [] [text "A recent project I worked on was ",
-              a [href "https://where2gocanada.ca/", Att.target "_blank"] [text "Where2GoCanada"],
-              text " which is a mobile web app whose purpose is to help Ukrainian refugees find the ideal place in Canada to move to, based on their employment experience."
+      , p [] [text "One project I worked on was Where2GoCanada which is a mobile web app whose purpose is to help Ukrainian refugees find the ideal place in Canada to move to, based on their employment experience."
               ]
       , p [ css [Css.height (px 20)]] []
       , p [] [text "Before this, I worked at "
@@ -172,7 +171,7 @@ viewWork =
         , text " which provides a great way of getting new products on the market, allowing for the general crowd to support farms and small businesses."
         ]
       ]
-    , button [ onClick DownloadFile, css buttonStyles ] [ text "Download CV" ]
+    --, button [ onClick DownloadFile, css buttonStyles ] [ text "Download CV" ]
     ]
 
 
@@ -180,13 +179,24 @@ viewHalloween : Html msg
 viewHalloween =
   div [ css [margin2 (px 0) (px 30)] ] [
     h2 [] [ text "Halloween"]
-    , h4 [] [ text "One of my hobbies is event planning, specifically for Halloween (though occasionally I will organize events for other holidays/themes). For the past few years I have organized elaborate Halloween parties with an accompanying app. The app presents the players with a story, along with tasks they must complete to progress the plot. Most of the 'tasks' are Halloween games I've come up with (complete with drinking rules), along with some riddle solving. The apps cannot be played without the real life activities, but one can still explore the apps with the codes provided on the github page. The web apps were designed for mobile, but still work fine on desktop." ]
-    , h3 [] [ a [ href "https://sachapf.github.io/halloween2021/", Att.target "_blank"] [ text "Halloween 2021"]]
+    , h4 [] [ text "One of my hobbies is event planning, specifically for Halloween (though occasionally I will organize events for other holidays/themes). For the past few years I have organized elaborate Halloween parties with an accompanying app. The app presents the players with a story, along with tasks they must complete to progress the plot. Most of the 'tasks' are Halloween games I've come up with (complete with drinking rules), along with some riddle solving. The apps cannot be played without the real life activities, but one can still explore the apps with the codes provided on the github page. The web apps were designed for mobile, but still work fine on desktop."]
+    , h4 [] [ text "Here I display information about iterations from several years ago. The newer versions of the apps are connected to a database, which must be reset to restart the games and allow them to be played again. While they cannot be played currently, the urls are: "
+            , a [ href "https://pf-physics.github.io/Halloween2025/", Att.target "_blank"] [ text "Halloween 2025"]
+            , text ", "
+            , a [ href "https://pf-physics.github.io/Halloween2024/", Att.target "_blank"] [ text "Halloween 2024"]
+            , text ", "
+            , a [ href "https://pf-physics.github.io/Halloween2022/", Att.target "_blank"] [ text "Halloween 2022"]
+            , text "."
+            ]
+    , h3 [] [ a [ href "https://pf-physics.github.io/halloween2021/", Att.target "_blank"] [ text "Halloween 2021"]]
     , h4 [] [ text "The premise of the party (sent to the guests before arriving) is as follows:"
             , p [] [ text "\"It is a dark and stormy night, you and your friends are heading for a Halloween party. Your car, which somehow fits 13 people, suddenly breaks down. There is no phone service this far out into the Danish woods. Luckily, you spy a looming manor in the distance. Making your way over you are greeted inside by a strange looking host...\""]
             , text "The host, a strange insect looking creature, greets the guests as if they were all old friends while admitting that his vision has deteriorated. He insists they stay for the evening. In order not to arouse suspicion of their human nature the group complies. The groups make their way through the rooms, entertaining their host's whims by playing games. When their host's back is turned they encounter his REAL friends, who express their disdain for the old man. Upon visiting the oracle, the guests learn that there is a strange dark power in the manor and urges them to investigate. If the guests lose enough health (by performing badly in games), they will become ghosts and attempt to turn the remaining guests into ghosts as well."
-            , text " The app was made in React." ]
-    , h3 [] [ a [ href "https://sachapf.github.io/halloween2020/", Att.target "_blank"] [ text "Halloween 2020"] ]
+            , text "The codes for the games can be found on the "
+            , a [ href "https://github.com/pf-physics/halloween2021", Att.target "_blank" ] [ text "GitHub" ]
+            , text " page."
+            ]
+    , h3 [] [ a [ href "https://pf-physics.github.io/halloween2020/", Att.target "_blank"] [ text "Halloween 2020"] ]
     , h4 [] [ text "In 2020, Halloween fell on a full moon and the event revolved around this fact. The guests are split up into two groups: mystics (consisting of witches, vampires and werewolves) and undead (skeletons, zombies, ghosts). Both groups are informed separately that, although the moon is full, its power is dull and that the OTHER group is responsible for its state. To solve this dispute, a series of challenges have been put forth to determine which group is superior. As they complete challenges, they find evidence that something much darker could be responsible for the moon's weakness. Depending on the species, different abilities are gained to interfere with the other team. The app was made in React" ]
     , h3 [] [ a [ href "https://play.google.com/store/apps/details?id=com.HalloweenInc.HalloweenApp", Att.target "_blank"] [ text "Halloween 2019"] ]
     , h4 [] [ text "Upon arriving, the guests are informed that different dimensions have been embedded into different rooms of the house. To find out why this happened, they must perform tasks in each dimension. As they investigate, they learn of a young girl who died in the house long ago, whose soul has gone missing..."
@@ -198,10 +208,12 @@ viewHobbies : Html msg
 viewHobbies =
   div [ css [margin2 (px 0) (px 30)] ]
     [ h1 [] [ text "Hobbies"]
-    , h4 [] [ text "I enjoy singing in choir or on my own. Most recently I was a part of the "
+    , h4 [] [ text "I enjoy singing in choir or on my own. Choirs I have been a part of include the "
             , a [ href "https://bachkoret.dk/", Att.target "_blank"] [ text "Københavns Bachkor" ]
-            , text ". Because of Corona, I had fewer concerts with them than planned but enjoyed performing the Bach Christmas Oratorio with them in 2019 and 2021."]
-    , h4 [] [ text "Previously, I sung with Les Muses Chorale while studying at McGill University."]
+            , text " and "
+            , a [href "https://lesmuseschorale.wordpress.com/", Att.target "_blank"] [ text "Les Muses Chorale"]
+            , text "."
+            ]
     , iframe
       [ Att.width 560
       , Att.height 100
@@ -211,16 +223,18 @@ viewHobbies =
       , Att.property "allowfullscreen" (Json.Encode.string "true")
       ]
       []
-    , h3 [] [ text "I also enjoy video making/editing"]
-    , iframe
-        [ Att.width 560
-        , Att.height 315
-        , css [ Css.marginBottom (Css.px 30)]
-        , src "https://www.youtube.com/embed/ZAe2AKLzVSw"
-        , Att.property "frameborder" (Json.Encode.string "0")
-        , Att.property "allowfullscreen" (Json.Encode.string "true")
+    , h3 [] [ text "I enjoy writing fiction and coming up with stories. This is a hobby I practice mostly through inventing the plots of my Halloween events, but I will submit my writing to competitions when I am made aware of them."]
+    , h3 [] [ text "Another hobby of mine is video making/editing. Here is a music video I made in 2018. While I have continued making shorter videos over the years, I haven't had the chance to take on as big of a project since then."]
+    , video
+        [ Att.controls True
+        , Att.width 640
         ]
-        []
+        [ source
+            [ Att.src "/Files/Tiny_video.mp4"
+            , Att.type_ "video/mp4"
+            ]
+            []
+        ]
     ]
 
 
